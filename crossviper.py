8875e0cdd3190e095b9c650fb25a01a26ee1084f
Morten <mortenhettler@web.de>
2017-11-14 22:12:16 +0100

    Add files via upload

--- crossviper.py
+++ crossviper.py
@@ -44,8 +44,8 @@ class ToolTip():
         if self.tipWindow or not self.text:
             return
         x, y, _cx, cy = self.widget.bbox('insert')
-        x = x + self.widget.winfo_rootx() + 27
-        y = y + cy + self.widget.winfo_rooty() + 27
+        x = x + self.widget.winfo_rootx() + 0
+        y = y + cy + self.widget.winfo_rooty() + 40
         self.tipWindow = tw = tk.Toplevel(self.widget)
         tw.wm_overrideredirect(1)
         tw.wm_geometry('+%d+%d' % (x, y))
